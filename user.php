4dabefbc7ab83106fb1ad72a96ba4037daca6738
milivojevic85 <misagrf@yahoo.com>
2020-01-27 12:17:15 +0100

    explaination added

--- user.php
+++ user.php
@@ -1,11 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>User</title>
+</head>
+<body>
+
 <?php 
+
 class User
 {
-	private $data = array();
+	private $data = array(); // a class attribute or property
 	
+	// a magic class method
 	public function __set($key, $value) {
 		$this->data[$key] = $value;
 	}
+	// a magic class method
 	public function __get($key) {
 		if (array_key_exists($key, $this->data)) {
 			return $this->data[$key];
@@ -13,9 +24,11 @@ class User
 	}
 }
 
-$user = new User();
+$user = new User(); // an instantination of the User class
 $user->first_name = "John";
 $user->last_name = "Doe";
-echo $user->first_name." ".$user->last_name."<br>";
+echo $user->first_name." ".$user->last_name."<br>"; // John Doe
 
 ?>
+</body>
+</html>
